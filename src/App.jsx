b9bcbbd905c66c62b3d9f3321454e6d508e6a3d0
ANAND <anandchandran3306@gmail.com>
2024-05-10 13:34:07 +0530

    card creation ongoing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import Users from "./components/Users"
+import ProductList from "./components/ProductList";
+import Users from "./components/Users";
 
 const App = () => {
   return (
     <div>
+      <ProductList />
       <Users />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
